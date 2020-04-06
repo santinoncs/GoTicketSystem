0511ffb2ffa6343b65360c8b208691784e1e2dc4
Santiago Núñez-Cacho <santiago.nunezcacho@softonic.com>
2020-04-06 18:47:34 +0200

    formatting

--- app/app.go
+++ app/app.go
@@ -1,27 +1,21 @@
 package app
 
-
-
-
-
 // Response : here you tell us what Response is
-type Response struct{
+type Response struct {
 	Success bool
 	Message string
 }
 
-
-
 // Job : here you tell us what Job is
-type Job struct{
-	ID int
-	Question string
+type Job struct {
+	ID           int
+	Question     string
 	ResponseChan chan Response
 }
 
-func jobProcess(priority int, question string) Job{
+func jobProcess(priority int, question string) Job {
 
-	responseChan1 := make (chan Response)
+	responseChan1 := make(chan Response)
 
 	j := Job{ID: priority, Question: question, ResponseChan: responseChan1}
 
@@ -29,40 +23,34 @@ func jobProcess(priority int, question string) Job{
 }
 
 // Post : escribo los jobs en jobs channel ya con los datos de prio y message
-func Post(priority int, question string,jobchan chan Job) Response{
+func Post(priority int, question string, jobchan chan Job) Response {
 
-
-	j := jobProcess(priority , question)
+	j := jobProcess(priority, question)
 
 	// aqui lanzo con go func el escribir en el channel de jobs
 
 	go func() {
-		
-	
+
 		if priority == 1 {
 
-			// escribo en jobsChan que trata con prioridad 1 , pero acepta jobs 
-			
+			// escribo en jobsChan que trata con prioridad 1 , pero acepta jobs
+
 			jobchan <- j
 
 		}
 		close(jobchan)
 	}()
-    return j.process()
+	return j.process()
 }
-
-
 
 func (j Job) process() Response {
 	// aqui leo el job, cojo el channel Response y me pongo a leer del channel  // hasta que  worker haya enviado el reponse que luego devolveré cuando     // llamen  a esta funcion
 
 	channelListenR := j.ResponseChan
-	
 
 	select {
-		case Response := <-channelListenR:
-			return Response
+	case Response := <-channelListenR:
+		return Response
 	}
-
 
 }
--- main.go
+++ main.go
@@ -2,46 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
 	app "github.com/santinoncs/GoTicketSystem/app"
+	"time"
 )
-
-
 
 // IncomingQuestion : here you tell us what IncomingQuestion is
 type IncomingQuestion struct {
-	Priority   int         
-	Question   string      
+	Priority int
+	Question string
 }
 
-func newWorker(j int,jobs <-chan app.Job) {
+func newWorker(j int, jobs <-chan app.Job) {
 
 	d := app.Response{
-			Success: true,
-			Message: "bye",
+		Success: true,
+		Message: "bye",
 	}
 
 	for j := range jobs {
-		
+
 		fmt.Println("worker", j, "started  job", j)
 		time.Sleep(time.Second)
 		fmt.Println("worker", j, "finished job", j)
 		j.ResponseChan <- d
 	}
-	
+
 }
 
 func main() {
 
-	
-
 	jobchan1 := make(chan app.Job)
 
 	for j := 1; j <= 2; j++ {
-        go newWorker(j, jobchan1)
-    }
+		go newWorker(j, jobchan1)
+	}
 
-	response := app.Post(1,"hola", jobchan1)
+	response := app.Post(1, "hola", jobchan1)
 
 	fmt.Println(response.Message)
 
